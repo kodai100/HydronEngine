cd3d309acc8059824f6efde47400e1f4f0711391
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-04-27 19:22:24 +0900

    debug layer stack

--- Hydron/src/Hydron/Core/LayerStack.cpp
+++ Hydron/src/Hydron/Core/LayerStack.cpp
@@ -1,12 +1,7 @@
 #include "pch.h"
-#include "LayerStack.h"
+#include "Hydron/Core/LayerStack.h"
 
 namespace Hydron {
-
-	LayerStack::LayerStack()
-	{
-		m_LayerInsert = m_Layers.begin();
-	}
 
 	LayerStack::~LayerStack()
 	{
@@ -19,7 +14,8 @@ namespace Hydron {
 
 	void LayerStack::PushLayer(Layer* layer)
 	{
-		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+		m_LayerInsertIndex++;
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
@@ -29,20 +25,18 @@ namespace Hydron {
 
 	void LayerStack::PopLayer(Layer* layer)
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-
-		if (it != m_Layers.end())
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
 			layer->OnDetach();
 			m_Layers.erase(it);
-			m_LayerInsert--;
+			m_LayerInsertIndex--;
 		}
 	}
 
 	void LayerStack::PopOverlay(Layer* overlay)
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
-
+		auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 		{
 			overlay->OnDetach();
