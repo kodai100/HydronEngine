74c2b4779c785f3e1e0f398972b24f3c5e8a189a
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-04-28 19:36:51 +0900

    implement buffer layout abstraction

--- Hydron/src/Hydron/Core/Application.cpp
+++ Hydron/src/Hydron/Core/Application.cpp
@@ -15,6 +15,29 @@ namespace Hydron {
 
 	Application* Application::s_Instance = nullptr;
 
+
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Hydron::ShaderDataType::Float:  return GL_FLOAT;
+			case Hydron::ShaderDataType::Float2: return GL_FLOAT;
+			case Hydron::ShaderDataType::Float3: return GL_FLOAT;
+			case Hydron::ShaderDataType::Float4: return GL_FLOAT;
+			case Hydron::ShaderDataType::Mat3:   return GL_FLOAT;
+			case Hydron::ShaderDataType::Mat4:   return GL_FLOAT;
+			case Hydron::ShaderDataType::Int:    return GL_INT;
+			case Hydron::ShaderDataType::Int2:   return GL_INT;
+			case Hydron::ShaderDataType::Int3:   return GL_INT;
+			case Hydron::ShaderDataType::Int4:   return GL_INT;
+			case Hydron::ShaderDataType::Bool:   return GL_BOOL;
+		}
+
+		HYDRON_CORE_ASSERT(false, "Unknown");
+		return 0;
+	}
+
+
 	Application::Application()
 	{
 		HYDRON_CORE_ASSERT(!s_Instance, "Application alreadt exists!");
@@ -30,17 +53,38 @@ namespace Hydron {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+			0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position" },
+				{ShaderDataType::Float4, "a_Color" }
+			};
+
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+		
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 
 		unsigned int indices[3] = {
@@ -53,12 +97,15 @@ namespace Hydron {
 			#version 330 core
 			
 			layout(location=0) in vec3 a_Position;
+			layout(location=1) in vec4 a_Color;
 			
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -69,10 +116,11 @@ namespace Hydron {
 			layout(location=0) out vec4 color;
 			
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
