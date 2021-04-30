cc384cafb47df6df9572c6163625d8fb7cf4f937
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-04-30 13:38:05 +0900

    fix phong shading

--- Hydron/src/Hydron/Renderer/Renderer.cpp
+++ Hydron/src/Hydron/Renderer/Renderer.cpp
@@ -31,10 +31,17 @@ namespace Hydron {
 	void Renderer::Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray, glm::mat4& transform)
 	{
 		shader->Bind();
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_Camera->GetViewProjection());
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_InvTransform", glm::inverse(transform));
+		
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ModelMatrix", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_InvModelMatrix", glm::inverse(transform));
+		
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewMatrix", m_Camera->GetViewMatrix());
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ProjectionMatrix", m_Camera->GetProjection());
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjectionMatrix", m_Camera->GetViewProjection());
+
 		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformFloat3("u_LightDirection", glm::normalize(glm::vec3(-0.5f, -0.5f, -0.5f)));
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformFloat3("u_LightColor", glm::normalize(glm::vec3(1.0f, 1.0f, 1.0f)));
+
 		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformFloat3("u_CameraPos", m_Camera->GetPosition());
 		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformFloat4("u_AmbientColor", glm::vec4(0.01f, 0.01f, 0.01f, 1.0f));
 
--- Sandbox/assets/shaders/TextureShaded.glsl
+++ Sandbox/assets/shaders/TextureShaded.glsl
@@ -5,8 +5,8 @@ layout(location=0) in vec3 a_Position;
 layout(location=1) in vec3 a_Normal;
 layout(location=2) in vec2 a_TexCoord;
 
-uniform mat4 u_Transform;
-uniform mat4 u_ViewProjection;
+uniform mat4 u_ModelMatrix;
+uniform mat4 u_ViewProjectionMatrix;
 
 out vec3 v_Position;
 out vec3 v_Normal;
@@ -14,10 +14,10 @@ out vec2 v_TexCoord;
 
 void main()
 {
-	v_Position = a_Position;
+	v_Position = vec3(u_ModelMatrix * vec4(a_Position, 1.0));
 	v_Normal =  normalize(a_Normal);
 	v_TexCoord = a_TexCoord;
-	gl_Position =  u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+	gl_Position =  u_ViewProjectionMatrix * u_ModelMatrix * vec4(a_Position, 1.0);
 }
 
 
@@ -26,10 +26,11 @@ void main()
 			
 layout(location=0) out vec4 color;
 
-uniform mat4 u_InvTransform;			
+uniform mat4 u_InvModelMatrix;			
 uniform sampler2D u_Texture;
 uniform vec4 u_Color;
 uniform vec3 u_LightDirection;
+uniform vec3 u_LightColor;
 uniform vec3 u_CameraPos;
 uniform vec4 u_AmbientColor;
 
@@ -40,11 +41,12 @@ in vec2 v_TexCoord;
 void main()
 {
 
-	vec3 eyeDir = -normalize(v_Position - u_CameraPos);
+	vec3 eyeDir = normalize(u_CameraPos - v_Position);
 
-	vec3  invLight  = normalize(u_InvTransform * vec4(-u_LightDirection, 0.0)).xyz;
-    vec3  invEye    = normalize(u_InvTransform * vec4(eyeDir, 0.0)).xyz;
+	vec3  invLight  = normalize(u_InvModelMatrix * vec4(-u_LightDirection, 0.0)).xyz;
+    vec3  invEye    = normalize(u_InvModelMatrix * vec4(eyeDir, 0.0)).xyz;
     vec3  halfLE    = normalize(invLight + invEye);
+
     float diffuse   = clamp(dot(v_Normal, invLight), 0.0, 1.0);
     float specular  = pow(clamp(dot(v_Normal, halfLE), 0.0, 1.0), 50.0);
     vec4  destColor = texture(u_Texture, v_TexCoord) * vec4(vec3(diffuse), 1.0) + vec4(vec3(specular), 1.0) + u_AmbientColor;
